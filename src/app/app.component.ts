014fd07477c181e9b436008d146574ee45dec44c
Demo learn Git <mhd.shawal@gmail.com>
2024-08-21 18:56:44 +0800

    fisrt commit angular

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -10,4 +10,45 @@ import { RouterOutlet } from '@angular/router';
 })
 export class AppComponent {
   title = 'ang-calculator';
-}
+
+
+  calValue:number =0;
+  funcT: any ='NoFunction';
+  calNumber: string  = 'noValue';
+
+  firstNumber: number = 0;
+  secondNumber: number = 0;
+
+
+
+
+  onClickValue(val:string, type: any)  {
+  if( type == 'number'){
+    this.onNumberClick(val);
+  }
+    else if (type == 'function'){
+      this.onFunctionClick(val);
+    }
+  }
+
+  onNumberClick(val:string){
+    if(this.calNumber != 'noValue'){
+    this.calNumber = this.calNumber + val;
+    }
+    else{
+      this.calNumber = val;
+    }
+    this.calValue = parseFloat(this.calNumber);
+  }
+
+  
+  onFunctionClick(val:string){
+    if(this.funcT == 'NoFunction'){
+        this.firstNumber = this.calValue;
+        this.calValue = 0; 
+        this.calNumber = 'noValue';
+        this.funcT = val;
+      }
+  }
+
+}   
